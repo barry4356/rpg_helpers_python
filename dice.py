597824d908813f247397f96066c1c27258ce6822
barry4356 <65548059+barry4356@users.noreply.github.com>
2021-09-14 16:53:24 -0400

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,12 @@
+#dice.py
+
+def roll_1d6():
+    return (random.randrange(6) + 1)
+    
+def roll_2d6():
+    val = roll_1d6() + roll_1d6()
+    return (val)
+    
+def roll_3d6():
+    val = roll_1d6() + roll_1d6() + roll_1d6()
+    return (val)
