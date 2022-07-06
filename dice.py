6f4fdfa46dc2f3e1473df73d47f4914293249d95
Barry <barryd@mail.usf.edu>
2022-07-06 12:18:12 +0000

    Added more dice option to dice roller

--- dice.py
+++ dice.py
@@ -11,3 +11,13 @@ def roll_2d6():
 def roll_3d6():
     val = roll_1d6() + roll_1d6() + roll_1d6()
     return (val)
+
+def roll_1d8():
+    return (random.randrange(8) + 1)
+
+def roll_2d8():
+    val = roll_1d8() + roll_1d8()
+    return (val)
+
+def roll_1d20():
+    return (random.randrange(20) + 1)
