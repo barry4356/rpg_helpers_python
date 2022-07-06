6c70901ed8aa328c0da7fa62ea69c429c00664d6
Barry <barryd@mail.usf.edu>
2022-07-06 20:36:26 +0000

    Add areyousure check

--- mausritter/mausritter_helper.py
+++ mausritter/mausritter_helper.py
@@ -3,55 +3,26 @@ import dice
 import utils
 import dungeon
 import combat
-        
-def dice_menu():
+
+def main_menu():     
     exit = False;
     while exit != True:
-        utils.print_header("Dice Roller")
-        print("How many dice to roll?")
-        print("1 - 1 6-sided Die")
-        print("2 - 2 6-sided Dice")
-        print("3 - 3 6-sided Dice")
-        print("4 - 1 8-sided Die")
-        print("5 - 2 8-sided Dice")
-        print("6 - 1 15-sided Die")
-        print("7 - 1 20-sided Die")
-        print("0 - Main Menu")
+        utils.print_header("MAUSRITTER HELPER")
+        print("Menu Selection...")
+
+        print("1 - Adventure Site Utils")
+        print("2 - Combat Helper")
+        print("0 - Exit")
+    
         val = utils.get_input()
-        print()
+        print("\n")
+    
         if(val == 0):
-            exit = True
-        elif(val == 1):
-            print(dice.roll_1d6())
-        elif(val == 2):
-            print(dice.roll_2d6())
-        elif(val == 3):
-            print(dice.roll_3d6())
-        elif(val == 4):
-            print(dice.roll_1d8())
-        elif(val == 5):
-            print(dice.roll_2d8())
-        elif(val == 6):
-            print(dice.roll_1d15())
-        elif(val == 7):
-            print(dice.roll_1d20())
-        print()
+            if utils.areyousure():
+                exit = True
+        elif (val == 1):
+            print(dungeon.menu())
+        elif (val == 2):
+            print(combat.menu())
 
-exit = False;
-while exit != True:
-    print ("\n===== MAUSRITTER HELPER =====\n")
-    print("Menu Selection...")
-
-    print("1 - Adventure Site Utils")
-    print("2 - Combat Helper")
-    print("0 - Exit")
-    
-    val = utils.get_input()
-    print("\n")
-    
-    if(val == 0):
-        exit = True
-    elif (val == 1):
-        print(dungeon.menu())
-    elif (val == 2):
-        print(combat.menu())
+main_menu()
--- utils.py
+++ utils.py
@@ -1,5 +1,22 @@
 #utils.py
 
+def areyousure():
+    exit = False;
+    while exit != True:
+        print("Are you Sure you want to Exit the program?")
+
+        print("1 - YES")
+        print("0 - NO")
+    
+        val = get_input()
+        print("\n")
+    
+        if(val == 1):
+            return True
+        else:
+            return False
+
+
 def print_header(title):
     print()
     print()
