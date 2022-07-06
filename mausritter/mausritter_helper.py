a8558de3eee9da4e8e0f60aa423ab009c53b65e2
Barry <barryd@mail.usf.edu>
2022-07-06 14:28:57 +0000

    Update dice tester

--- mausritter/mausritter_helper.py
+++ mausritter/mausritter_helper.py
@@ -11,6 +11,10 @@ def dice_menu():
         print("1 - 1 6-sided Die")
         print("2 - 2 6-sided Dice")
         print("3 - 3 6-sided Dice")
+        print("4 - 1 8-sided Die")
+        print("5 - 2 8-sided Dice")
+        print("6 - 1 15-sided Die")
+        print("7 - 1 20-sided Die")
         print("0 - Main Menu")
         val = utils.get_input()
         print()
@@ -22,6 +26,14 @@ def dice_menu():
             print(dice.roll_2d6())
         elif(val == 3):
             print(dice.roll_3d6())
+        elif(val == 4):
+            print(dice.roll_1d8())
+        elif(val == 5):
+            print(dice.roll_2d8())
+        elif(val == 6):
+            print(dice.roll_1d15())
+        elif(val == 7):
+            print(dice.roll_1d20())
         print()
 
 exit = False;
