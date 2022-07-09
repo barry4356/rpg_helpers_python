90a442483bae0db02f17ee734a621b3ec1c3e829
bdunham3 <bdunham3@gatech.edu>
2022-07-09 10:39:56 -0400

    Add more dice utils

--- dice.py
+++ dice.py
@@ -2,15 +2,18 @@
 import random
 import utils
 
+#Flips a coin
 def coin_flip():
     if random.randint(0, 1) == 1:
         return True
     else:
         return False
 
+#Rolls a custom-sized die
 def roll_custom(dice_size):
     return (random.randrange(dice_size)+1)
 
+#Roll D6(s)
 def roll_1d6():
     return (random.randrange(6) + 1)
     
@@ -22,6 +25,7 @@ def roll_3d6():
     val = roll_1d6() + roll_1d6() + roll_1d6()
     return (val)
 
+#Roll D8(s)
 def roll_1d8():
     return (random.randrange(8) + 1)
 
@@ -29,14 +33,23 @@ def roll_2d8():
     val = roll_1d8() + roll_1d8()
     return (val)
 
+#Roll D10(s)
 def roll_1d10():
     return (random.randrange(10) + 1)
 
+#TODO Make this use custom?
 def roll_1d15():
     return (random.randrange(15) + 1)
 
+#Roll D20(s)
 def roll_1d20():
     return (random.randrange(20) + 1)
+    
+#Roll on arbitrary-sized table, all values equal chance
+def roll_on_table(input_table):
+    table_size = len(input_table)
+    return(input_table[roll_custom(table_size)-1])
+
 
 def menu():
     exit = False;
