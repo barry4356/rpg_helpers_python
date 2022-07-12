e9814d3fa76eb70fab8df47ed9e44df52a6c4737
Barry <barryd@mail.usf.edu>
2022-07-12 20:10:40 +0000

    Fix typo

--- mausritter/tables/additional_tables.py
+++ mausritter/tables/additional_tables.py
@@ -5,7 +5,7 @@
 large=["cat","crow","crow","crow","crow","owl"]
 medium=["rat w/ d6 cleaver","rat w/ d6 cleaver","rat w/ d6 cleaver","snake","snake","snake"]
 small=[
-    "mouse w/ d6 sword","mouse w/ d8 Bow","mouse w/ d6 sword","mouse","frog w/ d10 spear",
+    "mouse w/ d6 sword","mouse w/ d8 Bow","mouse w/ d6 sword","mouse w/ d8 Bow","frog w/ d10 spear",
     "frog w/ d10 spear","frog w/ d10 spear","frog w/ d10 spear","faerie w/ d8 Silver Rapier","ghost"
 ]
 tiny=["spider","spider","spider","centipede","centipede","centipede"]
