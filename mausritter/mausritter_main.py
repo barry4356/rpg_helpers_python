f39e12489a49852aaf46c741dc133da95390a1b8
Barry <barryd@mail.usf.edu>
2022-07-12 15:35:08 +0000

    Add main logo

--- mausritter/mausritter_main.py
+++ mausritter/mausritter_main.py
@@ -8,7 +8,17 @@ import weather
 import encounters
 import hexmap_builder
 
-def main_menu():     
+def print_logo():
+    print("  __  __                      _ _   _            ")
+    print(" |  \/  | __ _ _   _ ___ _ __(_) |_| |_ ___ _ __ ")
+    print(" | |\/| |/ _` | | | / __| '__| | __| __/ _ \ '__|")
+    print(" | |  | | (_| | |_| \__ \ |  | | |_| ||  __/ |   ")
+    print(" |_|  |_|\__,_|\__,_|___/_|  |_|\__|\__\___|_|   ")
+    print("")
+
+
+def main_menu():
+    print_logo()
     func_list = [dungeon.menu,combat.menu,maus_roller.menu,weather.menu,hexmap_builder.menu]
     desc_list = ["Adventure Utils","Combat Helper","Maus Creator","Weather Utils","Hexmap Builder"]
     utils.menu(func_list,desc_list,"MAUSRITTER HELPER",True)
