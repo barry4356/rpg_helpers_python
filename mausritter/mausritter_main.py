bd633cba9d756098fb0b65fa4e1490d7e7168264
Barry Dunham <barry.a.dunham.iii@lmco.com>
2022-07-23 23:19:38 +0000

    Turn off lookup feature for now

--- mausritter/mausritter_main.py
+++ mausritter/mausritter_main.py
@@ -16,12 +16,12 @@ def print_help(args=[]):
     utils.print_header("Help - List of Commands")
     print("dmg\t Open enemy damage tracker menu".expandtabs(tabwidth))
     print("dmg [creature_name]\t Open enemy damage tracker for specified creature".expandtabs(tabwidth))
-    print("enctr\t Roll to see if there's a new encounter (1d6)".expandtabs(tabwidth))
+    print("enctr\t Roll to see if there's a new encounter".expandtabs(tabwidth))
     print("enctr -f\t Force an encounter".expandtabs(tabwidth))
     print("nwrm\t Create a new adventure area room".expandtabs(tabwidth))
     print("rolmaus\t Roll a new Player Character Maus".expandtabs(tabwidth))
-    print("rolmaus -h [number]\t Roll a new Henchman Maus (optional number to roll multiple)".expandtabs(tabwidth))
-    print("rolmaus -n [number]\t Roll a new Maus NPC (Optional number to roll multiple".expandtabs(tabwidth))
+    print("rolmaus -h [number]\t Roll a new Henchman Maus (number to roll multiple)".expandtabs(tabwidth))
+    print("rolmaus -n [number]\t Roll a new Maus NPC (number to roll multiple".expandtabs(tabwidth))
     print()
     print("exit\t Close Terminal".expandtabs(tabwidth))
     print()
@@ -36,12 +36,12 @@ def main_menu():
     func_list = [
         dungeon.menu, combat.menu, maus_roller.menu, 
         weather.menu, hexmap_builder.menu, hide_something.menu,
-        table_lookups.menu,mausritter_terminal
+        mausritter_terminal
     ]
     desc_list = [
         "Adventure Utils", "Combat Helper", "Maus Creator", 
         "Weather Utils", "Hexmap Builder", "Hide Something",
-        "Lookup Table","Mausritter Terminal"
+        "Mausritter Terminal"
     ]
     utils.menu(func_list,desc_list,"MAUSRITTER HELPER",True)
 
