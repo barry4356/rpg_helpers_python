5d1ea1061922bb144fe8168ef19e6584685b34de
barry4356 <65548059+barry4356@users.noreply.github.com>
2022-07-25 15:21:37 -0400

    Update additional_tables.py

--- mausritter/tables/additional_tables.py
+++ mausritter/tables/additional_tables.py
@@ -2,8 +2,8 @@
 
 #Pulled from Proposed Additional Rules (Experimental)
 
-large=["cat","crow","crow","crow","crow","owl"]
-medium=["rat w/ d6 cleaver","rat w/ d6 cleaver","rat w/ d6 cleaver","snake","snake","snake"]
+large=["cat","snake","snake","crow","crow","owl"]
+medium=["rat w/ d6 cleaver","rat w/ d6 cleaver","rat w/ d6 cleaver","frog","frog","frog"]
 small=[
     "mouse w/ d6 sword","mouse w/ d8 Bow","mouse w/ d6 sword","mouse w/ d8 Bow","frog w/ d10 spear",
     "frog w/ d10 spear","frog w/ d10 spear","frog w/ d10 spear","faerie w/ d8 Silver Rapier","ghost"
