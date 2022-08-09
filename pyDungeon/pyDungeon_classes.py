82a1900e0679a293c03978e3604e272dfdeb2616
Barry Dunham <barryd@mail.usf.edu>
2022-08-09 14:56:32 +0000

    Add Room class

--- pyDungeon/pyDungeon_classes.py
+++ pyDungeon/pyDungeon_classes.py
@@ -11,6 +11,11 @@ class Room:
         self.nodes = []
     def add_node(self,node):
         self.nodes.append(node.label)
+    def print_room():
+        print("Room: ["+str(self.room_number)+"]")
+        for node in self.nodes:
+            print("\tNode: ["+node+"]")
+        print("")
 
 class Node:
     """Defines a coordinate point that can be navigated through."""
@@ -22,3 +27,21 @@ class Node:
         self.room = ""
     def add_connection(self,node):
         self.connections.append([node.label])
+    def print_node():
+        print()
+        print("Node ["+self.label+"]")
+        print("Location ["+str(self.x)+","+str(self.y)+"]")
+        print("Connections:")
+        for connection in self.connections:
+            print("\t["+str(connection)+"]")
+        print("Room: ["+str(self.room)+"]")
+        print("==========")
+
+class Map:
+    """Defines a full set of map data."""
+    def __init__(self,x,y,label):
+        self.rooms = []
+        self.nodes = []
+        self.matrix = []
+        self.player_location = []
+        self.room = ""
