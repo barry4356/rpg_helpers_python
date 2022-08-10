408d2360c9a84a740b1d9b82881fd1ec31d1a48e
Barry Dunham <barryd@mail.usf.edu>
2022-08-10 18:45:56 +0000

    Added more fileops for saving data

--- pyDungeon/pyDungeon_fileops.py
+++ pyDungeon/pyDungeon_fileops.py
@@ -3,11 +3,22 @@
 import fileops
 
 def store_map(my_map):
-    print("store_map")
+    map_dict = {}
+    map_dict["map_name"]=my_map.map_name
+    map_dict["map_width"]=my_map.map_width
+    map_dict["map_height"]=my_map.map_height
+    map_dict["PC_location"]=str(my_map.player_location[0])+","+str(my_map.player_location[1])
     for room in my_map.rooms:
+        map_dict["room"+str(room.room_number)] = ("room"str(room.room_number)".room")
         store_room(room)
     for node in my_map.nodes:
+        map_dict["node"+str(node.label)] = ("node"str(node.label)".node")
         store_node(node)
+    map_dict["matrix"]=my_map.map_name+".matrix"
+    store_matrix(my_map.matrix)
+    map_dict["mask_matrix"]=my_map.map_name+".mask"
+    store_matrix(my_map.mask_matrix)
+    fileops.serialize_dict(map_dict,"maps/"+my_map.map_name+"/"+my_map.map_name+".map")
 
 def store_room(my_room):
     print("store_room")
