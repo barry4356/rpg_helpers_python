773141d005fd3e6e9440e76f12c1f810879bab21
barry4356 <barryd@mail.usf.edu>
2024-05-23 17:34:18 +0000

    AoF sim: Update gen empty weapon

--- AoF_sim/unit_class.py
+++ AoF_sim/unit_class.py
@@ -10,20 +10,24 @@ class unit():
             json.dump(self.stats, fp, indent=2)
 
     def from_file(self, filename):
+        # Load JSON
         empty_stats = self.gen_empty_stats()
         temp_stats = {}
         with open(filename) as json_file:
             temp_stats = json.load(json_file)
         self.stats = empty_stats
+        # Load Top Level Stats
         for mykey in self.stats:
             if mykey in temp_stats.keys():
                 self.stats[mykey] = temp_stats[mykey]
             elif mykey in empty_stats.keys():
                 self.stats[mykey] = empty_stats[mykey]
+        # Check for models
         if 'models' not in temp_stats.keys():
             return
         new_models = []
         empty_model = self.gen_empty_model()
+        # Load each model
         for temp_model in temp_stats['models']:
             new_model = {}
             for mykey in empty_model:
@@ -31,6 +35,7 @@ class unit():
                     new_model[mykey] = temp_model[mykey]
                 else:
                     new_model[mykey] = empty_model[mykey]
+            #TODO Handle each weapon
             new_models.append(new_model)
         self.stats['models'] = new_models
             
@@ -43,6 +48,8 @@ class unit():
         empty_weapon['hits'] = 0
         empty_weapon['ranged'] = False
         empty_weapon['melee'] = False
+        empty_weapon['ap'] = 0
+        return empty_weapon
 
     def gen_empty_model(self):
         empty_model = {}
@@ -53,6 +60,8 @@ class unit():
         empty_model['impact'] = 0
         empty_model['lance'] = False
         empty_model['counter'] = False
+        empty_model['furious'] = False
+        empty_model['furiouser'] = False
         return empty_model
 
     def gen_empty_stats(self):
