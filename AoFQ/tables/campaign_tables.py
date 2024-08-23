12dd401dc24920a82c7637802b67c560c2c6eebb
barry4356 <barryd@mail.usf.edu>
2024-08-23 19:47:50 +0000

    Add more narrative objectives

--- AoFQ/tables/campaign_tables.py
+++ AoFQ/tables/campaign_tables.py
@@ -52,10 +52,10 @@ primary_objectives_desc={
    "Target Defense" : 'Place one objective marker randomly at the centre of a table quarter or the centre of the table, which counts as the AI Goal. Heroes may use a skill action whilst within 1" of the marker and take a Str test. If passed, at the end of each round, if a hero is within 3" of the marker while enemies aren\'t, then it\'s seized, and it gets one token. The objective is completed once the marker has 4*X tokens (where X is the chosen difficulty level).',
    "Investigation" : 'Place two objective markers randomly at the centre of two different table quarters or the centre of the table, and the marker closest to the hero nearest to an enemy counts as its AI Goal. Heroes may use a skill action whilst within 1" of a marker, and if they pass a Dex test, the marker is removed. The objective is completed once both markers are removed.',
    "Delivery" : 'Place two delivery markers within 6" of two different random table corners (if the heroes are deployed in the same corner as a marker, move it to the centre of the table). When the heroes deploy, place one objective marker in their deployment zone, which counts as the AI Goal. Heroes within 1" of the marker may pick it up by using a skill action and passing a Wil test, and drop it within 1" if they are shaken. When the hero carrying the objective ends its activation within 3" of a delivery marker, remove that marker. The objective is completed once both markers are removed.',
-   "Last Call" : 'Heroes enter on one table edge. Place three hiding spot markers evenly spaced along the farthest table edge. Heroes may use a skill action whilst within 1" of a marker, and if they pass a Dex or Wil test, remove the marker and roll one die. On a 5+, or if it was the last marker, the heroes have found a hiding spot, and the objective is completed.',
-   "Doors of Tsevadraz" : '',
-   "Bog Battle" : '',
-   "Trapped Among Tombs" : '',
+   "Last Call" : 'Heroes enter on one table edge. Place three hiding spot markers evenly spaced along the farthest table edge. Heroes may use a skill action whilst within 1" of a marker, and if they pass a Dex or Wil test, remove the marker and roll one die. On a 5+, or if it was the last marker, the heroes have found a hiding spot, and the objective is completed. All heroes must be within 3" of the hiding spot to end the round.',
+   "Doors of Tsevadraz" : 'Place marker for gateway on far edge from heroes with 4*X tokens on it (where X is the difficulty level). This counts as the AI goal. Heroes may use a skill action within 1" of the marker and take a Str test to unlock the gate. If passed, at the end of the round if the hero is still within 3" of the marker, remove one token from it. Objective is complete when all tokens are removed. All heroes must be within 3" of marker to end the round.',
+   "Bog Battle" : 'Heroes deploy within 6" of table\'s center. Center of the table counds as the AI goal. When the last model from the second wave of reinforcements is killed, it drops an objective marker. heroes may use a skill action within 1" of the marker and if they pass a Dex check, the marker is removed and the objective is complete.',
+   "Trapped Among Tombs" : 'Heroes enter on table corner. Place 3 markers diagnolly evenly spaced across the board perpendicular to heroes\' entry corner. Markers count as AI goals, with priority being closest to heroes. Heroes may use skill action within 1" of a marker, and if the pass a Wil test, remove the marker and roll a die. On a 5+, or if it was the last marker, objective is complete.',
    "Silencing Sentries" : '',
    "Heart of Darkness" : ''
    
@@ -77,8 +77,6 @@ secondary_objectives_legacy=[
 
 secondary_objectives_narrative=[
         "Last Call",
-        "Doors of Tsevadraz",
-        "Bog Battle",
         "Trapped Among Tombs",
         "Silencing Sentries",
         "Heart of Darkness"
@@ -94,10 +92,8 @@ secondary_objectives_desc={
    "Sabotage" : 'Place one objective marker within 6" of the table corner furthest from one random hero, which counts as the AI Goal. Heroes may use a skill action whilst within 1" of the marker, and if they pass a Wil test, the marker is removed and the objective is completed.',
    "Safeguard" : 'Place one objective marker at the centre of the table, which counts as the AI Goal. Heroes may use a skill action whilst within 1" of the marker and take a Dex test. If passed, at the end of each round, if a hero is within 3" of the marker while enemies aren\'t, then it\'s seized, and it gets one token. The objective is completed once the marker has 2*X tokens (where X is the chosen difficulty level).',
    "Scavenge" : 'Place two objective markers within 6" of the two corners closest to one random hero, and the marker closest to the hero nearest to an enemy counts as its AI Goal. Heroes may use a skill action whilst within 1" of a marker, and if they pass a Dex test, remove the marker and roll one die. On a 4+, or if it was the last marker, the objective is completed.',
+   "Trapped Among Tombs" : 'Place one marker on furthest corner from heroes\' entry, which counts as an AI goal. Heroes suffer D3 stress whilst within 1" of the marker, removing the marker and completing the objective.',
    "Last Call" : '',
-   "Doors of Tsevadraz" : '',
-   "Bog Battle" : '',
-   "Trapped Among Tombs" : '',
    "Silencing Sentries" : '',
    "Heart of Darkness" : ''
 }
