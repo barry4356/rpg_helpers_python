62671efae6206e76955af5dc165c6a318b137477
Barry Dunham <dunhambarrya@gmail.com>
2025-04-30 19:16:35 -0400

    Add PDF => HTML conversion function

---
+++ AoF_sim/pdf2html.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3.7
+import sys
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice, TagExtractor
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.cmapdb import CMapDB
+from pdfminer.layout import LAParams
+from pdfminer.image import ImageWriter
+
+# main
+def convertPdf(inputfile, outputfile):
+    # debug option
+    debug = 0
+    # input option
+    password = b''
+    pagenos = set()
+    maxpages = 0
+    # output option
+    outfile = None
+    outtype = None
+    imagewriter = None
+    rotation = 0
+    stripcontrol = False
+    layoutmode = 'normal'
+    encoding = 'utf-8'
+    pageno = 1
+    scale = 1
+    caching = True
+    showpageno = True
+    laparams = LAParams()
+    #
+    PDFDocument.debug = debug
+    PDFParser.debug = debug
+    CMapDB.debug = debug
+    PDFPageInterpreter.debug = debug
+    #
+    rsrcmgr = PDFResourceManager(caching=caching)
+    outtype = 'html'
+    if outputfile:
+        outfp = open(outputfile, 'w', encoding=encoding)
+    else:
+        outfp = sys.stdout
+    device = HTMLConverter(rsrcmgr, outfp, scale=scale,
+                           layoutmode=layoutmode, laparams=laparams,
+                           imagewriter=imagewriter, debug=debug)
+    with open(inputfile, 'rb') as fp:
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(fp, pagenos,
+                                      maxpages=maxpages, password=password,
+                                      caching=caching, check_extractable=True):
+            page.rotate = (page.rotate+rotation) % 360
+            interpreter.process_page(page)
+    device.close()
+    outfp.close()
+    return
+
+convertPdf('list.pdf', 'test.html')
