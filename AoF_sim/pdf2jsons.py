ce2c7976a0c1d30343bf22452a7e03cce1a69dec
Barry Dunham <dunhambarrya@gmail.com>
2025-05-01 19:28:12 -0400

    Split sheet into dictionary for each unit

--- AoF_sim/pdf2jsons.py
+++ AoF_sim/pdf2jsons.py
@@ -10,6 +10,10 @@ from pdfminer.cmapdb import CMapDB
 from pdfminer.layout import LAParams
 from pdfminer.image import ImageWriter
 import re
+import json
+
+def update_string_at_position(original_string, index, new_char):
+    return original_string[:index] + new_char + original_string[index+1:]
 
 def convertPdf(inputfile, outputfile):
     # debug option
@@ -80,8 +84,68 @@ def cleanupHtmlTags(inputfile, outputfile):
                 #outFile.write(lines[index])
                 
 
+def cleanupUnitNameString(inputString):
+    outputString = inputString.split('[')[0]
+    outputString = outputString.split('>')[-1]
+    outputString = outputString.lower()
+    for index, chrctr in enumerate(outputString):
+        if index >= len(outputString)-1:
+            continue
+        if index == 0 or outputString[index-1] == ' ':
+            outputString = update_string_at_position(outputString, index, chrctr.upper())
+    outputString = outputString.replace(' ','').replace('\'','')
+    return outputString
+
+def numberOfModels(inputString):
+    numberOfModels = 0
+    numberOfModels = inputString.split('[')[-1].split(']')[0]
+    try:
+        numberOfModels = int(numberOfModels)
+    except ValueError as e:
+        print(f"Error: {e}")
+    return numberOfModels
+
+def unitString(inputString):
+    index = -1
+    outputString = inputString
+    while index < 0:
+        checkstring = inputString.split('<')[index]
+        index -= 1
+        print(checkstring)
+        if 'qua' in checkstring.lower() and 'def' in checkstring.lower():
+            outputString = checkstring
+            break
+    index = -1
+    while index < 0:
+        checkstring = inputString.split('>')[index]
+        index -= 1
+        if 'qua' in checkstring.lower() and 'def' in checkstring.lower():
+            outputString = checkstring
+            break
+    outputString = outputString.split('<')[0]
+    #outputString = inputString.split('<')[-2]
+    #outputString = outputString.split('>')[-1]
+    print(outputString)
+    return outputString
+
+def htmlToJson(inputfile, outputdir):
+    unit_string_dicts = []
+    with open(inputfile, 'r', errors='ignore') as inFile:
+        lines = inFile.readlines()
+        for index, line in enumerate(lines):
+            if 'qua' in line.lower() and 'def' in line.lower():
+                unit_string_dict = {}
+                unit_string_dict['name'] = cleanupUnitNameString(lines[index-1])
+                unit_string_dict['models'] = numberOfModels(lines[index-1])
+                unit_string_dict['string'] = unitString(line)
+                unit_string_dicts.append(unit_string_dict)
+    print(json.dumps(unit_string_dicts, indent=2))
+    
+    
+
 # TODO:
     # Function to read 2 lines of unit, and parse into JSON
         # cleaned html has 2 lines per unit, Qua +, Def + can be used to find valid unit lines?
 #convertPdf('list.pdf', 'test.html')
-cleanupHtmlTags('test.html', 'test_clean.html')
+#cleanupHtmlTags('test.html', 'test_clean.html')
+htmlToJson('test_clean.html', 'test')
