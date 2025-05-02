bd9bbdcc5b536dd1b15eac812182b22d71446aef
barry4356 <barryd@mail.usf.edu>
2025-05-02 12:55:16 +0000

    Add function to create unit from string

--- AoF_sim/unit_class.py
+++ AoF_sim/unit_class.py
@@ -1,34 +1,33 @@
 import json
 
-
 class unit():
     def __init__(self):
-        self.stats = self.gen_empty_stats()
+        self.data = self.gen_empty_data()
 
     def to_file(self, filename):
         with open(filename, 'w') as fp:
-            json.dump(self.stats, fp, indent=2)
+            json.dump(self.data, fp, indent=2)
 
     def from_file(self, filename):
         # Load JSON
-        empty_stats = self.gen_empty_stats()
-        temp_stats = {}
+        empty_data = self.gen_empty_data()
+        temp_data = {}
         with open(filename) as json_file:
-            temp_stats = json.load(json_file)
-        self.stats = empty_stats
-        # Load Top Level Stats
-        for mykey in self.stats:
-            if mykey in temp_stats.keys():
-                self.stats[mykey] = temp_stats[mykey]
-            elif mykey in empty_stats.keys():
-                self.stats[mykey] = empty_stats[mykey]
+            temp_data = json.load(json_file)
+        self.data = empty_data
+        # Load Top Level Data
+        for mykey in self.data:
+            if mykey in temp_data.keys():
+                self.data[mykey] = temp_data[mykey]
+            elif mykey in empty_data.keys():
+                self.data[mykey] = empty_data[mykey]
         # Check for models
-        if 'models' not in temp_stats.keys():
+        if 'models' not in temp_data.keys():
             return
         new_models = []
         empty_model = self.gen_empty_model()
         # Load each model
-        for temp_model in temp_stats['models']:
+        for temp_model in temp_data['models']:
             new_model = {}
             for mykey in empty_model:
                 # Populate model from file; use empty instance for missing keys
@@ -44,10 +43,18 @@ class unit():
                         if weapkey not in temp_weapon.keys():
                             temp_weapon[weapkey] = empty_weapon[weapkey]
             new_models.append(new_model)
-        self.stats['models'] = new_models
-            
+        self.data['models'] = new_models
+
+    def from_raw(self, unit_string):
+        #Takes raw input (parsed from army list pdf) and populates unit info
+        if 'qua' not in unit_string.lower() or 'def' not in unit_string.lower():
+            print('ERROR: Invalid Unit String')
+            print(unit_string)
+            return
+
     def to_string(self):
-        return (json.dumps(self.stats, indent=2))
+        #Outputs JSON string representing unit
+        return (json.dumps(self.data, indent=2))
 
     def gen_empty_weapon(self):
         empty_weapon = {}
@@ -56,6 +63,7 @@ class unit():
         empty_weapon['ranged'] = False
         empty_weapon['melee'] = False
         empty_weapon['ap'] = 0
+        empty_weapon['range'] = 0
         return empty_weapon
 
     def gen_empty_model(self):
@@ -71,8 +79,11 @@ class unit():
         empty_model['furiouser'] = False
         return empty_model
 
-    def gen_empty_stats(self):
-        empty_stats = {}
-        empty_stats['name'] = ''
-        empty_stats['models'] = []
-        return empty_stats
+    def gen_empty_data(self):
+        empty_data = {}
+        empty_data['name'] = ''
+        empty_data['models'] = []
+        empty_data['quality'] = 0
+        empty_data['defense'] = 0
+        empty_data['stats'] = {}
+        return empty_data
