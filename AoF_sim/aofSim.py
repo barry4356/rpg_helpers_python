d3e34bc29edcb4b577d96953442c0e0afe8df96d
barry4356 <barryd@mail.usf.edu>
2025-05-08 11:57:32 +0000

    Updates

--- AoF_sim/aofSim.py
+++ AoF_sim/aofSim.py
@@ -1,9 +1,16 @@
 import argparse
 import os
+import pdf2dicts
+import logging
+import json
 
 
-
+logger = logging.getLogger()
 def pdf2json(pdfFile, outDir):
+    dicts = pdf2dicts.convertPdf(pdfFile, outDir)
+    for unitDict in dicts:
+        pass
+        #Load into unit objects
     return
 
 
@@ -27,15 +34,23 @@ parser = argparse.ArgumentParser(description="Tool for deriving AoF odds and sta
 parser.add_argument('--pdf2json', action='store_true', help='Import PDF files and Export JSONs', required=False)
 parser.add_argument('-o', '--outputFolder', type=str, help='Directory to export results into', required=True)
 parser.add_argument('-v', '--verbose', action='store_true', help='Log everything we\'re doing to terminal and logfile', required=False)
+parser.add_argument('-i', '--inputFile', type=str, help='File to import into tool', required=False)
 
 args = parser.parse_args()
 
 outDir = None
+inFile = None
 logFile = None
 verbose = False
 
 if args.outputFolder:
     outDir = args.outputFolder
     os.makedirs(outDir, exist_ok=True)
+if args.inputFile:
+    inFile = args.inputFile
 
-
+if args.pdf2json:
+    if not inFile or not outDir:
+        logger.error('Input File and Output Directory Required for PDF2JSON Conversion')
+        exit()
+    pdf2json(inFile, outDir)
