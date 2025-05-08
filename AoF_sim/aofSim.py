fbdb2b903da2b1f222908431ee4a3e43966fda24
barry4356 <barryd@mail.usf.edu>
2025-05-08 19:13:08 +0000

    Add more config options

--- AoF_sim/aofSim.py
+++ AoF_sim/aofSim.py
@@ -32,9 +32,12 @@ parser = argparse.ArgumentParser(description="Tool for deriving AoF odds and sta
 #
 #import 2 lists and run full battery of statistical analyses
 parser.add_argument('--pdf2json', action='store_true', help='Import PDF files and Export JSONs', required=False)
+parser.add_argument('--melee-charge', action='store_true', help='Simulate a melee charge (attack and defense)', required=False)
 parser.add_argument('-o', '--outputFolder', type=str, help='Directory to export results into', required=True)
 parser.add_argument('-v', '--verbose', action='store_true', help='Log everything we\'re doing to terminal and logfile', required=False)
 parser.add_argument('-i', '--inputFile', type=str, help='File to import into tool', required=False)
+parser.add_argument('--attacker-json', type=str, help='File to import into attacker model', required=False)
+parser.add_argument('--defender-json', type=str, help='File to import into defender model', required=False)
 
 args = parser.parse_args()
 
