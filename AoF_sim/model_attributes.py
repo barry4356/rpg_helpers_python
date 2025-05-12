0f4e684ba451a4bab9d06cc4307385bcc658d3e5
barry4356 <barryd@mail.usf.edu>
2025-05-12 21:37:46 +0000

    Add missing attributes

--- AoF_sim/model_attributes.py
+++ AoF_sim/model_attributes.py
@@ -1,20 +1,24 @@
 # Attributes that apply per-model
 base_attributes = {
     'counter': False,
+    'elite': False:
     'fear': False,
     'fearless': False,
     'fast': False,
     'furious': False,
     'hero': False,
     'impact': 0,
+    'joust': False,
     'sergeant': False,
+    'specialist': False,
     'tough': 0,
 }
 
 # Attributes that apply per-weapon
 weapon_attributes = {
     'lance': False,
-    'poison': False
+    'poison': False,
+    'rending': False,
 }
 
 
